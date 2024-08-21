1ff29205bdf5d814a2efb460a0dbe461b85f638e
BBryanJ <boeing.bryan242@gmail.com>
2024-08-21 16:52:28 -0400

    feat(api): Replace ExpressJS with Hono + OpenAPI setup

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,46 @@
-import { createServer } from './server';
+import { serve } from '@hono/node-server';
+import { swaggerUI } from '@hono/swagger-ui';
+import { OpenAPIHono } from '@hono/zod-openapi';
+import { Hono } from 'hono';
 
-const PORT = process.env.PORT || 3002;
+const app = new OpenAPIHono();
 
-const server = createServer();
+app.doc('/docs', {
+  openapi: '3.0.0',
+  info: {
+    title: 'Polling App API',
+    version: '1.0.0',
+  },
+});
 
-server.listen(PORT, () => {
-  console.log(`API running on port ${PORT}`);
+app.get('/ui', swaggerUI({ url: '/docs' }));
+
+const api = new Hono().basePath('/api');
+
+api.get('/poll', (c) => {
+  return c.json({ message: 'GET all polls' });
+});
+
+api.get('/poll/:pollId', (c) => {
+  return c.json({ message: `GET poll by ID ${c.req.param('pollId')}` });
+});
+
+api.get('/poll/:pollId/option', (c) => {
+  return c.json({ message: `GET poll options by ID ${c.req.param('pollId')}` });
+});
+
+api.get('/poll/:pollId/option/:optionId', (c) => {
+  return c.json({
+    message: `GET poll option by ID ${c.req.param('optionId')} for poll ${c.req.param('pollId')}`,
+  });
+});
+
+app.route('/', api);
+
+const port = 3002;
+console.log(`Server is running on port ${port}`);
+
+serve({
+  fetch: app.fetch,
+  port,
 });
