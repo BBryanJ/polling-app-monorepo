9a92e4b075bf0aeae71e60c01712cc5d1ac23c2b
BBryanJ <boeing.bryan242@gmail.com>
2024-08-22 15:36:11 -0400

    feat(api): Add in POST, PUT, and DELETE routes

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,6 +2,7 @@ import { serve } from '@hono/node-server';
 import { swaggerUI } from '@hono/swagger-ui';
 import { OpenAPIHono } from '@hono/zod-openapi';
 import { Hono } from 'hono';
+import { logger } from 'hono/logger';
 
 const app = new OpenAPIHono();
 
@@ -15,6 +16,8 @@ app.doc('/docs', {
 
 app.get('/ui', swaggerUI({ url: '/docs' }));
 
+app.use(logger());
+
 const api = new Hono().basePath('/api');
 
 api.get('/poll', (c) => {
@@ -25,6 +28,18 @@ api.get('/poll/:pollId', (c) => {
   return c.json({ message: `GET poll by ID ${c.req.param('pollId')}` });
 });
 
+api.post('/poll', (c) => {
+  return c.json({ message: 'POST new poll' });
+});
+
+api.put('/poll/:pollId', (c) => {
+  return c.json({ message: `PUT poll by ID ${c.req.param('pollId')}` });
+});
+
+api.delete('/poll/:pollId', (c) => {
+  return c.json({ message: `DELETE poll by ID ${c.req.param('pollId')}` });
+});
+
 api.get('/poll/:pollId/option', (c) => {
   return c.json({ message: `GET poll options by ID ${c.req.param('pollId')}` });
 });
@@ -32,6 +47,24 @@ api.get('/poll/:pollId/option', (c) => {
 api.get('/poll/:pollId/option/:optionId', (c) => {
   return c.json({
     message: `GET poll option by ID ${c.req.param('optionId')} for poll ${c.req.param('pollId')}`,
+  });
+});
+
+api.post('/poll/:pollId/option', (c) => {
+  return c.json({
+    message: `POST new poll option for poll ${c.req.param('pollId')}`,
+  });
+});
+
+api.put('/poll/:pollId/option/:optionId', (c) => {
+  return c.json({
+    message: `PUT poll option by ID ${c.req.param('optionId')} for poll ${c.req.param('pollId')}`,
+  });
+});
+
+api.delete('/poll/:pollId/option/:optionId', (c) => {
+  return c.json({
+    message: `DELETE poll option by ID ${c.req.param('optionId')} for poll ${c.req.param('pollId')}`,
   });
 });
 
