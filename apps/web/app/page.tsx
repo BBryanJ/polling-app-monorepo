a26e1526ec14f2eddca5a1ca07d358ec70a0d79c
Bryan Boeing <BBryanJ@users.noreply.github.com>
2024-09-23 16:01:31 -0400

    feat(web): Connect database to web application (#5)
    * feat(web): Add navbar and footer to landing page
    
    * feat(web): Add app routes
    
    * feat(web): Create base for the poll form
    
    * feat(web): Connect database to web application

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,41 @@
 import { Code } from '@repo/ui/code';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-center p-24'>
-      <h1 className='text-4xl font-bold'>Polling app (Web)</h1>
-      <p className='text-md mt-4'>
-        Running from <Code className='font-mono font-bold'>apps/web</Code>
-      </p>
-    </main>
+    <>
+      <header className='flex items-center justify-between border-b-2 border-gray-200 p-4'>
+        <nav className='flex items-center justify-between p-4'>
+          <ul className='flex space-x-4'>
+            <li>
+              <a href='/'>Home</a>
+            </li>
+            <li>
+              <Link href='/new-poll'>Create Poll</Link>
+            </li>
+          </ul>
+        </nav>
+        <div className='flex items-center space-x-4'>
+          <span>Login</span>
+          <span>Sign Up</span>
+        </div>
+      </header>
+      <main className='flex min-h-screen flex-col items-center justify-center gap-4 p-24'>
+        <h1 className='text-4xl font-bold'>Polling app (Web)</h1>
+        <h2 className='text-3xl font-semibold'>Landing Page</h2>
+        <p className='text-md'>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi illo
+          velit unde id voluptas et saepe, nostrum rerum amet eius aspernatur
+          incidunt maxime consequuntur doloremque perferendis aliquam ratione,
+          ad atque?
+        </p>
+      </main>
+      <footer className='flex items-center justify-between border-t-2 border-gray-200 p-4'>
+        <div className='flex items-center space-x-4'>
+          <span>Terms</span>
+          <span>Privacy</span>
+        </div>
+      </footer>
+    </>
   );
 }
---
+++ apps/web/app/polls/[pollId]/page.tsx
@@ -0,0 +1,40 @@
+import Link from 'next/link';
+
+export default async function PollPage({
+  params,
+}: {
+  params: { pollId: string };
+}) {
+  const { pollId } = params;
+  const poll = await prisma?.poll.findFirst({
+    where: {
+      id: Number(pollId),
+    },
+    include: {
+      options: true,
+    },
+  });
+
+  if (!poll) {
+    return (
+      <div className='flex h-screen items-center justify-center'>
+        Poll does not exist
+      </div>
+    );
+  }
+
+  return (
+    <div className='flex h-screen flex-col items-center justify-center'>
+      <h2 className='text-2xl font-bold'>{poll.title}</h2>
+      <div className='flex flex-col gap-2'>
+        {poll.options.map((option) => (
+          <div key={option.optionId}>
+            {option.name} ({option.votes}{' '}
+            {option.votes === 1 ? 'vote' : 'votes'})
+          </div>
+        ))}
+      </div>
+      <Link href='/polls'>Back to Polls</Link>
+    </div>
+  );
+}
