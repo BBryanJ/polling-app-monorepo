dd5e4d1e22b35ba9b439fd188217565fd99d13ff
BBryanJ <boeing.bryan242@gmail.com>
2024-09-23 15:17:55 -0400

    feat(web): Connect database to web application

--- apps/web/app/polls/[pollId]/page.tsx
+++ apps/web/app/polls/[pollId]/page.tsx
@@ -1,8 +1,40 @@
-export default function PollPage({ params }: { params: { pollId: string } }) {
+import Link from 'next/link';
+
+export default async function PollPage({
+  params,
+}: {
+  params: { pollId: string };
+}) {
   const { pollId } = params;
+  const poll = await prisma?.poll.findFirst({
+    where: {
+      id: Number(pollId),
+    },
+    include: {
+      options: true,
+    },
+  });
+
+  if (!poll) {
+    return (
+      <div className='flex h-screen items-center justify-center'>
+        Poll does not exist
+      </div>
+    );
+  }
+
   return (
-    <>
-      <h1>Page for Poll {pollId}</h1>
-    </>
+    <div className='flex h-screen flex-col items-center justify-center'>
+      <h2 className='text-2xl font-bold'>{poll.title}</h2>
+      <div className='flex flex-col gap-2'>
+        {poll.options.map((option) => (
+          <div key={option.optionId}>
+            {option.name} ({option.votes}{' '}
+            {option.votes === 1 ? 'vote' : 'votes'})
+          </div>
+        ))}
+      </div>
+      <Link href='/polls'>Back to Polls</Link>
+    </div>
   );
 }
