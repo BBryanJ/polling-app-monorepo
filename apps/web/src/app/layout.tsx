7d68ee8f6b8b12e6625fd374240edb5fc1cda228
BBryanJ <boeing.bryan242@gmail.com>
2024-10-03 14:17:14 -0400

    feat(ui): Add @repo/ui to web app

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
-import './globals.css';
+import '@repo/ui/globals.css';
 import Header from '../components/Header';
 
 const geistSans = localFont({
